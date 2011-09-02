d0c0caf2cf4c155b6b6d43062241155af4267250
Peter Zijlstra <a.p.zijlstra@chello.nl>
2011-09-02 14:29:27 +0200

    sched: teach migrate_disable about atomic contexts
     <NMI>  [<ffffffff812dafd8>] spin_bug+0x94/0xa8
     [<ffffffff812db07f>] do_raw_spin_lock+0x43/0xea
     [<ffffffff814fa9be>] _raw_spin_lock_irqsave+0x6b/0x85
     [<ffffffff8106ff9e>] ? migrate_disable+0x75/0x12d
     [<ffffffff81078aaf>] ? pin_current_cpu+0x36/0xb0
     [<ffffffff8106ff9e>] migrate_disable+0x75/0x12d
     [<ffffffff81115b9d>] pagefault_disable+0xe/0x1f
     [<ffffffff81047027>] copy_from_user_nmi+0x74/0xe6
     [<ffffffff810489d7>] perf_callchain_user+0xf3/0x135
    
    Now clearly we can't go around taking locks from NMI context, cure
    this by short-circuiting migrate_disable() when we're in an atomic
    context already.
    
    Add some extra debugging to avoid things like:
    
      preempt_disable()
      migrate_disable();
    
      preempt_enable();
      migrate_enable();
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1314967297.1301.14.camel@twins
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/n/tip-wbot4vsmwhi8vmbf83hsclk6@git.kernel.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5200,6 +5200,17 @@ void migrate_disable(void)
 	unsigned long flags;
 	struct rq *rq;
 
+	if (in_atomic()) {
+#ifdef CONFIG_SCHED_DEBUG
+		p->migrate_disable_atomic++;
+#endif
+		return;
+	}
+
+#ifdef CONFIG_SCHED_DEBUG
+	WARN_ON_ONCE(p->migrate_disable_atomic);
+#endif
+
 	preempt_disable();
 	if (p->migrate_disable) {
 		p->migrate_disable++;
@@ -5248,6 +5259,16 @@ void migrate_enable(void)
 	unsigned long flags;
 	struct rq *rq;
 
+	if (in_atomic()) {
+#ifdef CONFIG_SCHED_DEBUG
+		p->migrate_disable_atomic--;
+#endif
+		return;
+	}
+
+#ifdef CONFIG_SCHED_DEBUG
+	WARN_ON_ONCE(p->migrate_disable_atomic);
+#endif
 	WARN_ON_ONCE(p->migrate_disable <= 0);
 
 	preempt_disable();
